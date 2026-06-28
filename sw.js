8b1c734cad867eb2c64af67e150ea0b68493294c
yougav <gavinwstewart@icloud.com>
2026-06-28 22:14:54 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION='onward-cache-2026-06-28e';
-const CORE=['./','onward.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png','icon-512-maskable.png'];
+const VERSION='onward-cache-2026-06-28g';
+const CORE=['./','onward.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png','icon-512-maskable.png','launch-iphone17pro.png'];
 self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(VERSION).then(function(c){return c.addAll(CORE).catch(function(){});}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==VERSION)return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){var req=e.request;if(req.method!=='GET')return;e.respondWith(caches.match(req).then(function(hit){var net=fetch(req).then(function(res){if(res&&res.status===200&&res.type==='basic'){var copy=res.clone();caches.open(VERSION).then(function(c){c.put(req,copy);});}return res;}).catch(function(){return hit;});return hit||net;}));});
